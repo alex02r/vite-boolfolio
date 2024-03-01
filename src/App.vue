c3392de7dae1e2c40381a452c9fc59b15ceed0e2
alex02r <alex.giochi02@gmail.com>
2024-03-01 16:51:22 +0100

    [UPGRADE] Methods per la chiamata API che restituisce tutti i projects

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <script>
+import projects from "./components/projects.vue";
+import { store } from "./store";
 export default {
-  
+  components:{
+    projects
+  },
+  data() {
+    return {
+      store
+    }
+  },
+  methods: {
+    getProjects(){
+      axios.get(store.endpoint).then(response => {
+        store.projects = response.data.results
+      })
+    }
+  },
 }
 </script>
 <template lang="">
+
 </template>
 <style lang="scss">
   @use './styles/generals.scss'
