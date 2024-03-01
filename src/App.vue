7fa6441a03360455180a30949a2d954e19c75c47
alex02r <alex.giochi02@gmail.com>
2024-03-01 17:00:22 +0100

    [UPGRADE] Eseguita la chiamata API per ricevere tutti i projects

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script>
 import projects from "./components/projects.vue";
 import { store } from "./store";
+import axios from 'axios';
 export default {
   components:{
     projects
@@ -10,17 +11,21 @@ export default {
       store
     }
   },
+  created() {
+    this.getProjects();
+  },
   methods: {
     getProjects(){
       axios.get(store.endpoint).then(response => {
         store.projects = response.data.results
+        console.log(response.data.results);
       })
     }
   },
 }
 </script>
 <template lang="">
-
+  <projects></projects>
 </template>
 <style lang="scss">
   @use './styles/generals.scss'
