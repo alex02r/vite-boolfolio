4e9a74673efe941963f914531da52f3abf45a501
alex02r <alex.giochi02@gmail.com>
2024-03-01 17:06:59 +0100

    [UPGRADE] Visualizzazione di tutti i project

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,11 +1,23 @@
 <script>
+import { store } from '../store';
 export default {
-  
+  data() {
+    return {
+      store
+    }
+  },
 }
 </script>
 <template lang="">
-  <div>
-    
+  <div class="container my-4">
+    <div class="row">
+      <div class="col-12">
+        <h1>All Projects</h1>
+      </div>
+      <div class="col-3" v-for="(project, index) in store.projects" :key="index">
+        prova
+      </div>
+    </div>
   </div>
 </template>
 <style lang="">
