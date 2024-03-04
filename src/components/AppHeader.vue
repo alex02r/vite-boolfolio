e6f4c5b6ff10c50dc08c4a86aeb86a03f9d25c41
alex02r <alex.giochi02@gmail.com>
2024-03-04 14:51:42 +0100

    [UPGRADE] Creazione header components

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<script>
+import { store } from '../store';
+export default {
+    data() {
+        return {
+            store
+        }
+    },
+}
+</script>
+<template lang="">
+    <header>
+        <div class="container">
+            <div class="row justify-content-between align-items-center">
+                <div class="col-3">
+                    <h1>Portfolio</h1>
+                </div>
+                <div class="col-6">
+                    <ul class="list-unstyled d-flex gap-3">
+                        <li v-for="(item, index) in store.list_menu" :key="index">
+                            <router-link :to="{ name: item.name }" class="nav-link">{{ item.label}}</router-link>    
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+<style lang="">
+    
+</style>
