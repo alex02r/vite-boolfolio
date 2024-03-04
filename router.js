d908108691747724e3a0d7db2324ee3152e41f6c
alex02r <alex.giochi02@gmail.com>
2024-03-04 14:37:01 +0100

    [FIX] cambiata l cartella e fixato l'import

--- router.js
+++
@@ -1,9 +0,0 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-
-const router = createRouter({
-    hystory: createWebHashHistory(),
-
-    routes: [
-        {path: '/', components: AppHome}
-    ]
-})
