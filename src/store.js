7859c512be7b6788128334768e3c9d9789f542b2
alex02r <alex.giochi02@gmail.com>
2024-03-04 14:51:26 +0100

    [UPGRADE] Aggiunta del menu list

--- src/store.js
+++ src/store.js
@@ -3,5 +3,15 @@ import { reactive } from "vue";
 export const store = reactive({
     endpoint: 'http://127.0.0.1:8000/api/projects',
     API_URL: 'http://127.0.0.1:8000',
-    projects: []
+    projects: [],
+    list_menu: [
+        {
+            name: 'home',
+            label: 'Home'
+        },
+        {
+            name: 'projects',
+            label: 'Progetti'
+        }
+    ]
 })
