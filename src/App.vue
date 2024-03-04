c7c5368dd9e2411f8eeee75a41068fe8b33092a9
alex02r <alex.giochi02@gmail.com>
2024-03-04 13:04:26 +0100

    [UPGRADE] Aggiunta della paginazione

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,25 @@ export default {
   },
   data() {
     return {
-      store
+      store,
+      currentPage: 1,
+      lastPage: null
     }
   },
   created() {
     this.getProjects();
   },
   methods: {
-    getProjects(){
-      axios.get(store.endpoint).then(response => {
-        store.projects = response.data.results
+    getProjects(page_number){
+      axios.get(store.endpoint, {
+        params:{
+          page: page_number
+        }
+      }).then(response => {
+          
+        store.projects = response.data.results.data
+        this.currentPage = response.data.results.current_page
+        this.lastPage = response.data.results.last_page
         console.log(response.data.results);
       })
     }
@@ -26,6 +35,20 @@ export default {
 </script>
 <template lang="">
   <ProjectCard></ProjectCard>
+  <div class="container mb-3">
+    <div class="row">
+      <div class="col-12">
+        <div class="d-flex justify-content-center align-items-center gap-2">
+          <button class="btn btn-sm btn-light border" :class="currentPage == 1 ? 'disabled' : ''" @click="getProjects(currentPage - 1)">
+            <i class="fas fa-arrow-left"></i>
+          </button>
+          <button class="btn btn-sm btn-light border" :class="currentPage == lastPage ? 'disabled' : ''" @click="getProjects(currentPage + 1)">
+            <i class="fas fa-arrow-right"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <style lang="scss">
   @use './styles/generals.scss'
