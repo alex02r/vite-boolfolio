bf52fb493a4977837b72c64716bea3f0c5cb3f62
alex02r <alex.giochi02@gmail.com>
2024-03-04 15:48:43 +0100

    [UPGRADE] Rotta per page not found

--- src/router.js
+++ src/router.js
@@ -1,13 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
 import AppHome from './pages/AppHome.vue'
 import ProjectCard from './pages/ProjectCard.vue'
+import AppNotFound from './pages/AppNotFound.vue'
 
 const router = createRouter({
     history: createWebHistory(),
 
     routes: [
         {path: '/', name: 'home', component: AppHome},
-        {path: '/projects', name: 'projects', component: ProjectCard}
+        {path: '/projects', name: 'projects', component: ProjectCard},
+        {path: '/:pathMatch(.*)*', name: 'NotFound', component: AppNotFound}
     ]
 })
 
