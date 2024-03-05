48b7995657a1eb642ac3ccef397b4354776cc8c6
alex02r <alex.giochi02@gmail.com>
2024-03-05 16:11:24 +0100

    [UPGRADE] Chiamata axios per recuperare il project dallo slug

--- src/pages/AppSingleProject.vue
+++ src/pages/AppSingleProject.vue
@@ -1,6 +1,26 @@
 <script>
+import axios from 'axios'
+import { store } from "../store";
 export default {
-    
+    data() {
+        return {
+            project: null
+        }
+    },
+    created() {
+        this.getProject()
+    },
+    methods: {
+        getProject(){
+            axios.get(`${store.endpoint}/${this.$route.params.slug}`).then( response => {
+                if (response.data.success) {
+                    this.project = response.data.project
+                }else{
+                    this.$route.push({ name: 'NotFound'})
+                }
+            })
+        }
+    },
 }
 </script>
 <template lang="">
