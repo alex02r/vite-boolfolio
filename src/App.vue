bcf06c4ebad6e6b5c948e866fa36e071de90cb77
alex02r <alex.giochi02@gmail.com>
2024-03-05 15:24:27 +0100

    [FIX] Remove comment

--- src/App.vue
+++ src/App.vue
@@ -3,31 +3,7 @@ import AppHeader from "./components/AppHeader.vue";
 export default {
   components:{
     AppHeader
-  },
-  /* data() {
-    return {
-      store,
-      currentPage: 1,
-      lastPage: null
-    }
-  },
-  created() {
-    this.getProjects();
-  },
-  methods: {
-    getProjects(page_number){
-      axios.get(store.endpoint, {
-        params:{
-          page: page_number
-        }
-      }).then(response => {
-          
-        store.projects = response.data.results.data
-        this.currentPage = response.data.results.current_page
-        this.lastPage = response.data.results.last_page
-      })
-    } 
-  },*/
+  }
 }
 </script>
 <template lang="">
