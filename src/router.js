ec077c3a0c605f6f370ac2b4fc1409fcf8d1e019
alex02r <alex.giochi02@gmail.com>
2024-03-05 15:56:37 +0100

    [FIX] Cambiato il nome di un component e aggiunta una ","

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import AppHome from './pages/AppHome.vue'
 import AppProjects from './pages/AppProjects.vue'
-import AppSingleProject from './pages/AppSingleProject.vue'
+import SingleProject from './pages/AppSingleProject.vue'
 import AppNotFound from './pages/AppNotFound.vue'
 
 const router = createRouter({
@@ -10,8 +10,8 @@ const router = createRouter({
     routes: [
         {path: '/', name: 'home', component: AppHome},
         {path: '/projects', name: 'projects', component: AppProjects},
-        {path: '/projects/:slug', name:'single-project' component: AppSingleProject}
-        {path: '/:pathMatch(.*)*', name: 'NotFound', component: AppNotFound},
+        {path: '/projects/:slug', name:'single-project', component: SingleProject},
+        {path: '/:pathMatch(.*)*', name: 'NotFound', component: AppNotFound}
     ]
 })
 
