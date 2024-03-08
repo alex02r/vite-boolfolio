552727c99589b3114df517b914c8256bd16f0fb2
alex02r <alex.giochi02@gmail.com>
2024-03-08 17:46:58 +0100

    [UPGRADE] Creazione pagina thankyou

---
+++ src/pages/AppThanks.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    
+}
+</script>
+<template lang="">
+    <div class="container my-5">
+        <div class="row justify-content-center">
+            <div class="col-6">
+                <h1>Grazie per averci contattato</h1>
+                <h5>Email inviata con successo</h5>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="">
+    
+</style>
